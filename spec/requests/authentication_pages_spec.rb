5d7ab92c640815aacda0f4e41e91d71de3ab326c
Luiz Camargo <luiz.camargo@nhs.com.br>
2013-06-03 10:28:25 -0300

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -46,6 +46,37 @@ describe "Authentication" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
       end
+      
+      describe "micropost count" do
+        before { visit root_path }
+        let!(:older_micropost) do
+          FactoryGirl.create(:micropost, user: user, created_at: 1.day.ago)
+        end
+        
+        it { should have_content('micropost')  }
+        
+        let!(:newer_micropost) do
+          FactoryGirl.create(:micropost, user: user, created_at: 1.hour.ago)
+        end
+        
+        it { should have_content('microposts') }
+        
+
+      end
+      
+      # describe "pagination" do
+      #         let(:user) { FactoryGirl.create(:user) }
+      #         before  do
+      #           sign_in user 
+      #           visit root_path
+      #         end
+      #         
+      #         30.times { FactoryGirl.create(:micropost, user: user, content: "Consectetur adipiscing elit") }
+      #         
+      #         it "should paginate the feed" do
+      #           page.should have_selector("div.pagination")
+      #         end
+      #       end    
     end
   end
   
@@ -100,6 +131,16 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+      
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+        
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
       end
       describe "in the Microposts controller" do
         describe "submitting to the create action" do
@@ -112,6 +153,19 @@ describe "Authentication" do
           specify { response.should redirect_to(signin_path) }
         end
       end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+        
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+      
     end
     
     describe "as non-admin user" do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,7 +32,29 @@ describe "Static pages" do
           page.should have_selector("li##{item.id}", text: item.content)
         end
       end
+      describe "followers/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
+  
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+        sign_in user
+        visit root_path
+      end
+    end
+    
+
   end
 
   describe "Help page" do
